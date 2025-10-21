b811b4ce497b33e4c5099bac3735b73524f82b85
Stellios Bonadurer <stelliosbonadurer@gmail.com>
2025-10-21 11:53:08 -0600

    Create tutoring app

---
+++ Tutoring_App/accounts/forms.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ("age",)
+        
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
+        
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
